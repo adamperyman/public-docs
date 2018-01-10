29659dde2f4baccdbe508b1d18bfd959b8e4da09
Adam Peryman <adam.peryman@gmail.com>
2018-01-10 13:33:38 +1300

    Now using bash instead of sh and removing relative paths.

--- setup-docker.sh
+++ setup-docker.sh
@@ -4,18 +4,25 @@
 # Author: Adam Peryman <adam.peryman@gmail.com>
 # Tested on Ubuntu 16.04 LTS
 
-if [ -z ${NEW_USER+x} ]; then
-  echo "ENV var NEW_USER is undefined."
+if [ -z ${USER_NAME+x} ]; then
+  echo "ENV var USER_NAME is undefined."
 
   echo -n "Please enter new username: "
-  read NEW_USER
+  read USER_NAME
 fi
 
-if [ -z ${NEW_PASSWORD+x} ]; then
-  echo "ENV var NEW_PASSWORD is undefined."
+if [ -z ${USER_EMAIL+x} ]; then
+  echo "ENV var USER_EMAIL is undefined."
+
+  echo -n "Please enter the new user's email: "
+  read USER_EMAIL
+fi
+
+if [ -z ${USER_PASS+x} ]; then
+  echo "ENV var USER_PASS is undefined."
 
   echo -n "Please enter new password: "
-  read NEW_PASSWORD
+  read USER_PASS
 fi
 
 if [ -z ${SSH_ENCRYPTION_ALGORITHM+x} ]; then
@@ -33,14 +40,14 @@ apt_install_cmd="apt-get $apt_switches install"
 if [ -d "/root/.ssh/" ]; then
   if [ "$SSH_ENCRYPTION_ALGORITHM" == "ed25519" ]; then
     echo "Creating SSH keys for ROOT user using $SSH_ENCRYPTION_ALGORITHM algorithm.."
-    ssh-keygen -t ed25519 -a 100 -N "" -f id_ed25519 # Similar complexity to RSA 4096 but significantly smaller.
+    ssh-keygen -t ed25519 -a 100 -N "" -c $USER_EMAIL -f $HOME/.ssh/id_ed25519 # Similar complexity to RSA 4096 but significantly smaller.
   elif [ "$SSH_ENCRYPTION_ALGORITHM" == "rsa" ]; then
     echo "Creating SSH keys for ROOT user using RSA algorithm.."
-    ssh-keygen -t rsa -b 4096 -o -a 100 -N "" -f id_rsa
+    ssh-keygen -t rsa -b 4096 -o -a 100 -N "" -c $USER_EMAIL -f $HOME/.ssh/id_rsa
   else
     echo "Unknown SSH_ENCRYPTION_ALGORITHM, defaulting to RSA."
     echo "Creating SSH keys for ROOT user using RSA algorithm.."
-    ssh-keygen -t rsa -b 4096 -o -a 100 -N "" -f id_rsa
+    ssh-keygen -t rsa -b 4096 -o -a 100 -N "" -c $USER_EMAIL -f $HOME/.ssh/id_rsa
   fi
 
   echo "Finished creating SSH keys."
@@ -51,7 +58,7 @@ $apt_update_cmd && $apt_install_cmd whois git apt-utils
 
 # Get password hash.
 echo "Creating hashed password.."
-HASHED_PASSWORD=$(mkpasswd -m sha-512 $NEW_PASSWORD)
+HASHED_PASSWORD=$(mkpasswd -m sha-512 $USER_PASS)
 
 if [ -z ${HASHED_PASSWORD+x} ]; then
   echo "Failed to create hashed password.."
@@ -61,28 +68,28 @@ fi
 echo "Hashed password created successfully."
 
 # Create user.
-echo "Creating user: $NEW_USER.."
-if useradd -m -p $HASHED_PASSWORD -s /bin/bash $NEW_USER; then
-  echo "User: $NEW_USER created successfully."
+echo "Creating user: $USER_NAME.."
+if useradd -m -p $HASHED_PASSWORD -s /bin/bash $USER_NAME; then
+  echo "User: $USER_NAME created successfully."
 else
-  echo "Failed to create user: $NEW_USER."
+  echo "Failed to create user: $USER_NAME."
   exit 1
 fi
 
 echo "Assigning group permissions.."
-if usermod -aG sudo $NEW_USER; then
-  echo "Successfully assigned $NEW_USER to sudo group."
+if usermod -aG sudo $USER_NAME; then
+  echo "Successfully assigned $USER_NAME to sudo group."
 else
-  echo "Failed to assign $NEW_USER to sudo group."
+  echo "Failed to assign $USER_NAME to sudo group."
   exit 1
 fi
 
-echo "Logging in as $NEW_USER.."
-su $NEW_USER
+echo "Logging in as $USER_NAME.."
+su $USER_NAME
 if [ $? -eq 0 ]; then
-  echo "Successfully logged in as $NEW_USER."
+  echo "Successfully logged in as $USER_NAME."
 else
-  echo "Failed to login as $NEW_USER, current user is $NEW_USER."
+  echo "Failed to login as $USER_NAME, current user is $USER_NAME."
   exit 1
 fi
 
@@ -138,26 +145,26 @@ fi
 
 echo "Finished installing docker."
 
-echo "Assigning $NEW_USER to docker group.."
+echo "Assigning $USER_NAME to docker group.."
 sudo groupadd docker
-if sudo usermod -aG docker $NEW_USER; then
-  echo "Successfully added $NEW_USER to docker group."
+if sudo usermod -aG docker $USER_NAME; then
+  echo "Successfully added $USER_NAME to docker group."
 else
-  echo "Failed to add $NEW_USER to docker group."
+  echo "Failed to add $USER_NAME to docker group."
   # Don't need to exit here, investigate manually.
 fi
 
 # Setup SSH, use Ed25519 (new) or RSA depending on your needs.
 if [ "$SSH_ENCRYPTION_ALGORITHM" == "ed25519" ]; then
   echo "Creating SSH keys using $SSH_ENCRYPTION_ALGORITHM algorithm.."
-  ssh-keygen -t ed25519 -a 100 -N "" -f id_ed25519 # Similar complexity to RSA 4096 but significantly smaller.
+  ssh-keygen -t ed25519 -a 100 -N "" -c $USER_EMAIL -f $HOME/.ssh/id_ed25519
 elif [ "$SSH_ENCRYPTION_ALGORITHM" == "rsa" ]; then
   echo "Creating SSH keys using RSA algorithm.."
-  ssh-keygen -t rsa -b 4096 -o -a 100 -N "" -f id_rsa
+  ssh-keygen -t rsa -b 4096 -o -a 100 -N "" -c $USER_EMAIL -f $HOME/.ssh/id_rsa
 else
   echo "Unknown SSH_ENCRYPTION_ALGORITHM, defaulting to RSA."
   echo "Creating SSH keys using RSA algorithm.."
-  ssh-keygen -t rsa -b 4096 -o -a 100 -N "" -f id_rsa
+  ssh-keygen -t rsa -b 4096 -o -a 100 -N "" -c $USER_EMAIL -f $HOME/.ssh/id_rsa
 fi
 
 echo "Finished creating SSH keys."
@@ -167,20 +174,20 @@ echo "Finished creating SSH keys."
 sudo $apt_update_cmd && sudo $apt_install_cmd vim-gnome
 
 # Amix's .vimrc.
-if git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime; then
-  sh ~/.vim_runtime/install_awesome_vimrc.sh
+if git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime; then
+  bash $HOME/.vim_runtime/install_awesome_vimrc.sh
 
   # AP's custom settings.
-  mkdir -p ~/dev
-  git clone https://github.com/x0bile/vim-settings.git ~/dev/vim-settings
-  sh ~/dev/vim-settings/setup.sh
+  mkdir -p $HOME/dev
+  git clone https://github.com/x0bile/vim-settings.git $HOME/dev/vim-settings
+  bash $HOME/dev/vim-settings/setup.sh
 else
   echo "Failed to get Amix's .vimrc, didn't setup AP's custom settings."
 fi
 
 # Output.
 echo "You should add the following PUBLIC key to any services that require it, e.g. Github..\n"
-cat ~/.ssh/id_$SSH_ENCRYPTION_ALGORITHM.pub
+cat $HOME/.ssh/id_$SSH_ENCRYPTION_ALGORITHM.pub
 
-echo "We're done here, please logout and back in to refresh user groups for user: $NEW_USER."
+echo "We're done here, please logout and back in to refresh user groups for user: $USER_NAME."
 echo "Have a wonderful day! :)"
